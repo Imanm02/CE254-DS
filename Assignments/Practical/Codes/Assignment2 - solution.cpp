7c3b1eecc6f35e169327671cd85018e82cebdd94
Imanm02 <imanm1381@gmail.com>
2023-12-19 20:06:16 +0330

    Clean Code - Assignment2

--- Assignments/Practical/Codes/Assignment2 - solution.cpp
+++ Assignments/Practical/Codes/Assignment2 - solution.cpp
@@ -4,53 +4,50 @@
 using namespace std;
 
 int main() {
-    int array_lengh;
-    cin >> array_lengh;
-    int array[ array_lengh ];
-    int nights[ array_lengh ];
-    list <int> peaks;
-    for (int i = 0; i < array_lengh; i++) {
-        cin >> array[ i ];
-        nights[ i ] = 0;
+    int arrayLength;
+    cin >> arrayLength;
+    int heights[arrayLength];
+    int nights[arrayLength] = {0};
+    list<int> peaks;
+
+    for (int i = 0; i < arrayLength; i++) {
+        cin >> heights[i];
     }
-    int j = 0;
-    if (array[ 0 ] >= array[ 1 ]) {
+
+    if (heights[0] >= heights[1]) {
         peaks.push_front(0);
-    } else {
-        for (j = 1; j < array_lengh - 1; j++) {
-            if (array[ j - 1 ] < array[ j ] && array[ j ] >= array[ j + 1 ]) {
-                peaks.push_front(j);
-                break;
-            }
+    }
+
+    int startIndex = (heights[0] >= heights[1]) ? 1 : 0;
+    for (int i = startIndex; i < arrayLength - 1; i++) {
+        if (heights[i] > heights[i - 1] && heights[i] >= heights[i + 1]) {
+            peaks.push_front(i);
+            break;
         }
     }
+
     int rising = 1;
-    for (int i = j + 1; i < array_lengh; i++) {
-        if (array[ i - 1 ] < array[ i ]) {
+    for (int i = startIndex + 1; i < arrayLength; i++) {
+        if (heights[i] > heights[i - 1]) {
             rising++;
-            if (array[ peaks.front() ] < array[ i ]) {
-                if (rising > nights[ peaks.front() ]) nights[ i ] += rising;
-                else nights[ i ] += nights[ peaks.front() ] + 1;
-                while (peaks.size() > 0) {
-                    if (array[ i ] > array[ peaks.front() ]) {
-                        if (nights[ peaks.front() ] + 1 > nights[ i ]) nights[ i ] = nights[ peaks.front() ] + 1;
-                        peaks.pop_front();
-                    }
-                    else break;
+            if (heights[i] > heights[peaks.front()]) {
+                nights[i] = max(nights[peaks.front()] + 1, rising);
+                while (!peaks.empty() && heights[i] > heights[peaks.front()]) {
+                    nights[i] = max(nights[i], nights[peaks.front()] + 1);
+                    peaks.pop_front();
                 }
-                if (peaks.size() == 0) nights[ i ] = 0;
+                if (peaks.empty()) nights[i] = 0;
             } else {
-                nights[ i ] += rising;
+                nights[i] = rising;
             }
             peaks.push_front(i);
         } else {
-            nights[ i ] = 1;
+            nights[i] = 1;
             rising = 1;
         }
     }
-    int max_of_nights = 0;
-    for (int i = 0; i < array_lengh; i++) {
-        if (nights[ i ] > max_of_nights) max_of_nights = nights[ i ];
-    }
-    cout << max_of_nights << endl;
+
+    int maxNights = *max_element(nights, nights + arrayLength);
+    cout << maxNights << endl;
+    return 0;
 }
