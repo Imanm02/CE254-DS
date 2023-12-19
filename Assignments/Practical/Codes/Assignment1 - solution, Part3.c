0fe598d837d5817b96871d6655c7aa87fd6560b7
Imanm02 <imanm1381@gmail.com>
2023-12-19 20:01:25 +0330

    Clean Code - Assignment1, part3

--- Assignments/Practical/Codes/Assignment1 - solution, Part3.c
+++ Assignments/Practical/Codes/Assignment1 - solution, Part3.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 
 int main() {
-    long long int n;
-    long long int max;
-    long long int Shomarandeh;
-    scanf("%lld %lld", &n, &max);
-    long long int m = 0;
-    long long int numbers[n];
-    for (long long int i = 0; i < n; i++)
-    {
-        scanf("%lld ", &m);
-        numbers[i] = m;
+    long long n, maxSum;
+    scanf("%lld %lld", &n, &maxSum);
+    long long numbers[n], sum = 0, subarrayCount = 0;
+
+    // خواندن اعداد
+    for (long long i = 0; i < n; i++) {
+        scanf("%lld ", &numbers[i]);
     }
-    long long int k = 0;
-    long long int j = 1;
-    long long int sum = 0;
-    sum += numbers[k];
-    for ( k = 0; k < n; k++) {
-        while (sum <= max && j <= n) {
-            sum = sum + numbers[j];
-            Shomarandeh = Shomarandeh + j - k;
-            j++;
+
+    for (long long start = 0, end = 0; start < n; start++) {
+        // اضافه کردن عناصر تا زمانی که مجموع کمتر یا مساوی maxSum باشد
+        while (end < n && sum + numbers[end] <= maxSum) {
+            sum += numbers[end++];
         }
-        sum = sum - numbers[k];
+
+        // اضافه کردن تعداد زیرآرایه‌های ممکن با شروع از عنصر فعلی
+        subarrayCount += end - start;
+
+        // کاستن عنصر شروع از مجموع
+        sum -= numbers[start];
     }
-    printf("%lld ", Shomarandeh);
+
+    printf("%lld ", subarrayCount);
+    return 0;
 }
